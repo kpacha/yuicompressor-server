f153e8126caf32895482ba24c680a4c8244eab66
Alejandro Pérez <alexgt9@gmail.com>
2014-02-06 15:04:52 +0100

    Added the max form size as parameter an toggle cache default

--- src/main/java/com/github/kpacha/yuicompressorserver/Configuration.java
+++ src/main/java/com/github/kpacha/yuicompressorserver/Configuration.java
@@ -8,57 +8,78 @@ import org.apache.commons.cli.ParseException;
 import org.apache.log4j.Logger;
 
 public class Configuration {
-    private static final int DEFAULT_PORT = 8080;
-    private static Logger logger = Logger.getLogger(Configuration.class);
+	private static final int DEFAULT_PORT = 8080;
+	private static final String DEFAULT_ALGORITHM = "md5";
+	private static final int DEFAULT_MAX_FORM_SIZE = 1024000;
+	private static Logger logger = Logger.getLogger(Configuration.class);
 
-    private CommandLine cmd;
-    private int port;
-    private boolean cacheEnabled;
+	private CommandLine cmd;
+	private int port;
+	private boolean cacheEnabled;
+	private int max_form_size;
 
-    public Configuration(String[] args) throws ParseException {
-	initCmdLine(args);
-	port = initPort();
-	cacheEnabled = initCacheEnabled();
-    }
-
-    public boolean isCacheEnabled() {
-	return cacheEnabled;
-    }
-
-    public int getPort() {
-	return port;
-    }
-
-    private void initCmdLine(String[] args) throws ParseException {
-	CommandLineParser parser = new GnuParser();
-	Options options = getOptions();
-	cmd = parser.parse(options, args);
-    }
-
-    private Options getOptions() {
-	Options options = new Options();
-
-	options.addOption("p", true, "port (default 8080)");
-	options.addOption("c", false, "disable cache");
-
-	return options;
-    }
-
-    private int initPort() {
-	Integer port;
-	if (cmd.hasOption("p")) {
-	    port = Integer.parseInt(cmd.getOptionValue("p"));
-	} else {
-	    port = DEFAULT_PORT;
+	public Configuration(String[] args) throws ParseException {
+		initCmdLine(args);
+		port = initPort();
+		cacheEnabled = initCacheEnabled();
+		max_form_size = initMaxFormSize();
 	}
-	logger.info("Selected port: " + port);
-	return port;
-    }
 
-    private boolean initCacheEnabled() {
-	boolean isEnabled = !cmd.hasOption("c");
-	logger.info("Cache enabled: " + isEnabled);
-	return isEnabled;
-    }
+	public boolean isCacheEnabled() {
+		return cacheEnabled;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public int getMaxFormSize() {
+		return max_form_size;
+	}
+
+	private void initCmdLine(String[] args) throws ParseException {
+		CommandLineParser parser = new GnuParser();
+		Options options = getOptions();
+		cmd = parser.parse(options, args);
+	}
+
+	private Options getOptions() {
+		Options options = new Options();
+
+		options.addOption("p", true, "port (default 8080)");
+		options.addOption("c", false, "enable cache");
+		options.addOption("h", true, "hashing algorithm");
+		options.addOption("m", true, "max form size");
+
+		return options;
+	}
+
+	private int initPort() {
+		Integer port;
+		if (cmd.hasOption("p")) {
+			port = Integer.parseInt(cmd.getOptionValue("p"));
+		} else {
+			port = DEFAULT_PORT;
+		}
+		logger.info("Selected port: " + port);
+		return port;
+	}
+
+	private boolean initCacheEnabled() {
+		boolean isEnabled = cmd.hasOption("c");
+		logger.info("Cache enabled: " + isEnabled);
+		return isEnabled;
+	}
+
+	private int initMaxFormSize() {
+		Integer max_form_size;
+		if (cmd.hasOption("m")) {
+			max_form_size = Integer.parseInt(cmd.getOptionValue("m"));
+		} else {
+			max_form_size = DEFAULT_MAX_FORM_SIZE;
+		}
+		logger.info("Max form size: " + max_form_size);
+		return max_form_size;
+	}
 
 }
--- src/test/java/com/github/kpacha/yuicompressorserver/ConfigurationTest.java
+++ src/test/java/com/github/kpacha/yuicompressorserver/ConfigurationTest.java
@@ -8,12 +8,12 @@ public class ConfigurationTest extends TestCase {
 
     public void testCacheEnabled() throws ParseException {
 	Configuration config = new Configuration(new String[] {});
-	assertTrue(config.isCacheEnabled());
+	assertFalse(config.isCacheEnabled());
     }
 
     public void testCacheIsDisabled() throws ParseException {
 	Configuration config = new Configuration(new String[] { "-c" });
-	assertFalse(config.isCacheEnabled());
+	assertTrue(config.isCacheEnabled());
     }
 
     public void testDefaultPort() throws ParseException {
@@ -26,10 +26,21 @@ public class ConfigurationTest extends TestCase {
 	assertEquals(80, config.getPort());
     }
 
+    public void testDefaultMaxFormSize() throws ParseException {
+	Configuration config = new Configuration(new String[] {});
+	assertEquals( 1024000, config.getMaxFormSize());
+    }
+
+    public void testCustomMaxFormSize() throws ParseException {
+	Configuration config = new Configuration(new String[] { "-m", "1024" });
+	assertEquals(1024, config.getMaxFormSize());
+    }
+
     public void testAllIn() throws ParseException {
 	Configuration config = new Configuration(new String[] { "-p", "80",
-		"-c" });
+		"-c", "-m", "1024" });
 	assertEquals(80, config.getPort());
-	assertFalse(config.isCacheEnabled());
+	assertTrue(config.isCacheEnabled());
+	assertEquals(1024, config.getMaxFormSize());
     }
 }
